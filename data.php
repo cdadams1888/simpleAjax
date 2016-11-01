ac9a7af59ff35b1ad9e0f7b612ec0779015b1d96
Christopher Adams <cdadams1888@yahoo.com>
2016-11-01 13:10:23 -0400

    Add files via upload

---
+++ data.php
@@ -0,0 +1,16 @@
+<?php
+
+$names[] = 'Christopher';
+$names[] = 'Dewy';
+$names[] = 'Scott';
+$names[] = 'Mike';
+$names[] = 'Robert';
+$names[] = 'Artistotle';
+$count   = 1;
+foreach($names as $name)
+{
+    echo $count. ' '. $name. '<br />';
+    $count++;
+}
+
+
